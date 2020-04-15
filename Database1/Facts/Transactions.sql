f20647cd27039e17fd3403ef15e1186bc85e7746
=dhruv joshi <dhruvj@contata.com>
2020-04-15 16:58:07 +0530

    add changes

--- Database1/Facts/Transactions.sql
+++ Database1/Facts/Transactions.sql
@@ -4,7 +4,8 @@
     [Person1ID] INT NULL,
 	[Person2ID] INT NULL,
 	amount MONEY NOT NULL, 
-    [status] NCHAR(10) NULL,
+    [statusId] INT NOT NULL,
 	CONSTRAINT P1ID FOREIGN KEY (Person1ID) REFERENCES DimPerson(PersonID),
-    CONSTRAINT P2ID FOREIGN KEY (Person2ID) REFERENCES DimPerson(PersonID)
+    CONSTRAINT P2ID FOREIGN KEY (Person2ID) REFERENCES DimPerson(PersonID), 
+    CONSTRAINT [FK_Transactions_ToTable] FOREIGN KEY (StatusID) REFERENCES DimStatus(Id)
 )
