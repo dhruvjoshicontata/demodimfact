6a7f9e90ed1a07cf833381bb893824b8b9d9d01a
=dhruv joshi <dhruvj@contata.com>
2020-04-15 16:43:27 +0530

    Add project files.

---
+++ Database1/Dim/DimPerson.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].DimPerson
+(
+	[PersonID] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50)  NULL, 
+    [Age] INT NULL, 
+    [Gender] BIT NULL
+)
---
+++ Database1/Facts/Transactions.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Transactions]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Person1ID] INT NULL,
+	[Person2ID] INT NULL,
+	amount MONEY NOT NULL, 
+    [status] NCHAR(10) NULL,
+	CONSTRAINT P1ID FOREIGN KEY (Person1ID) REFERENCES DimPerson(PersonID),
+    CONSTRAINT P2ID FOREIGN KEY (Person2ID) REFERENCES DimPerson(PersonID)
+)
